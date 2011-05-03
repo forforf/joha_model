28e33dd016e1c4a4784857eaedfdfa8724b0a530
Dave M <dmarti21@gmail.com>
2011-05-03 20:28:10 +0000

    WIP - Graphing and Create Node working

---
+++ lib/joha_model.rb
@@ -0,0 +1,165 @@
+require 'couchrest'
+require 'tinkit'
+require 'burp'
+require 'kinkit'
+require 'jsivt_grapher'
+
+class JohaModel
+  #rename this when going to test or production
+  #TODO: create config to do the renaming
+  CouchDB = CouchRest.database!("http://127.0.0.1:5984/joha_test_data/")
+  #data operations are defined in tinkit/node_element/operations
+  JohaDataDefn = {:id => :static_ops,
+                  :label => :replace_ops,
+                  :description => :replace_ops,
+                  :links => :key_list_ops,
+                  :parents => :list_ops,
+                  :notes => :list_ops,
+                  :history => :list_ops}
+
+  attr_reader :tinkit_class, :grapher, :graphs
+
+  #ToDo: BaseClass that can support old Bufs Model and new Joha Model
+  #Bufs Model should be backwards compatible
+  #Joha Model should be able to be drop and replace
+  #Though won't work with Bufs formatted persistent storage
+  def initialize(tinkit_class_name, user_id)#, data_field_defs = {})
+    #key_field = data_field_defs[:key_field] || :id  #maybe my_category?
+    #parents_field = data_field_defs[:parents_field] || :parents
+    @key_field = :id
+    @parents_field = :parents
+    #TODO, need to fix formatter to accept custom field ops
+    joha_env = TinkitNodeFactory.env_formatter("couchrest",
+                                               tinkit_class_name,
+                                               user_id,
+                                               CouchDB.uri,
+                                               CouchDB.host)
+
+    #hack until formatter is fixed
+    joha_env[:data_model][:field_op_set] = JohaDataDefn
+
+    @tinkit_class = TinkitNodeFactory.make(joha_env)
+    
+    #TODO Figure out approach to deal with subsets of records
+    #to handle huge data repositories
+    all_joha_tinkits = @tinkit_class.all
+
+    #I've given up on native tinkit class into burp for now
+    all_joha_node_data = all_joha_tinkits.map{|node| node._user_data}
+  
+    #transforms an array of hashes to a hash with key field
+    #pointing to hashed data of node
+    burped_tinkits = Burp.new(all_joha_node_data, @key_field)
+
+    #finds relationships between nodes
+    tinkit_kins = Kinkit.new(burped_tinkits, @parents_field)
+    @graphs = tinkit_kins.uniq_digraphs
+    #parent child relationship data  #adds field :children
+    joha_relationships = tinkit_kins.parent_child_maps
+
+    joha_relationship_structure = {:id => @key_field, 
+                                  :name_key => :label,
+                                  :children =>:children }
+
+     #p full_data.methods
+
+    @grapher = JsivtGrapher.new(joha_relationships, joha_relationship_structure)
+  end
+
+  def tree_graph(top_node, depth=4)
+    #check if node exists?
+    @grapher.to_tree(top_node, depth)
+  end
+  
+  #TODO Move to forforf_rgl_adjacency
+  #list unique graphs with a guess
+  #as to the best top node as an id
+  #each node is unique so any node
+  #will uniquely identify a given graph
+  #The model uses this to let the user choose
+  #which graph to browse, and since each node
+  #is unique, the graph can be selected using that
+  #top node.
+  def graphs_with_roots
+    graphs_with_roots = {}
+    #sort_graphs = @graphs.sort {|a,b| a.size <=> b.size }
+    @graphs.each do |graph|
+      best_top_nodes = graph.best_top_vertices
+      best_top_node = best_top_nodes.min{ |n1,n2| graph.in_degree(n1) <=> graph.in_degree(n2) }
+      graphs_with_roots[best_top_node] = graph
+    end
+    return graphs_with_roots
+  end
+
+  #Method maps (TODO: I'm sure there's a better way)
+  def create_node(params)
+    node = @tinkit_class.new(params)
+    node.__save
+    #TODO: What if params includes attachments?
+  end
+
+  def update_node(params)
+    node = @tinkit_class.get(params[@key_field])
+    raise "No node to update for #{@key_field} => #{params[@key_field]}."\
+          "Maybe you wanted to create a new node instead?" unless node
+    #TODO: What if params includes attachments?
+
+    #for joha fields, use <field>_add method
+    #for all other fields, put them in user_data
+  end
+
+  def select_node(id)
+    @tinkit_class.get(id)
+  end
+
+  def destroy_node(id)
+    node = @tinkit_class.get(id)
+    node.__destroy_node
+  end
+
+  def download_attachment(node_id, att_name)
+    node = @tinkit_class.get(node_id)
+    node.get_raw_data(att_name)
+  end
+
+  def list_attachments(id)
+    node = @tinkit_class.get(id)
+    node.attached_files
+  end
+
+  def list(id, param)
+    if param == :attachments
+      list_attachments(id)
+    else
+      node = @tinkit_class.get(id)
+      data = node.__send__(:param)
+      data.respond_to?(:each) ? data : [data]
+    end
+  end
+
+  #The data definition defines what 
+  #'subtract' means in the conext of that data item
+  #item_ids can be singular or plural
+  #But if data item is singular, use singular
+  # param_data_item = ['a', 'b', 'c']
+  # param_data_item_subtract('a') = ['b', 'c']
+  # param_data_item_subtract(['a']) = ['b', 'c']
+  # param_data_item_subtract(['a', 'b', 'c']) = nil
+  # param_data_item = :foo
+  # param_data_item_subtract(:foo) = nil
+  # param_data_item_subtract([:foo]) = :foo
+  def remove_item(id, param, item_ids)
+    remove_command = "#{param}_subtract".to_sym
+    node = @tinkit_class.get(id)
+    node.__send__(remove_command, item_ids)
+  end    
+ 
+  #similar to remove above, TODO: Extensive testing needed
+  #to ensure all parameter items behave as expected
+  def add_item(id, param, item_ids)
+    add_command = "#{param}_add".to_sym
+    node = @tinkit_class.get(id)
+    node.__send__(add_command, item_ids)
+  end
+
+end
--- spec/joha_model_spec.rb
+++ spec/joha_model_spec.rb
@@ -1,7 +1,54 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+
+
 describe "JohaModel" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  before(:each) do
+    @joha_class_name = "JohaTestClass"
+    @joha_user = "joha_test_user"
+    @jm = JohaModel.new(@joha_class_name, @joha_user)
+  end
+
+  it "initializes" do
+    @jm.should_not == nil
+    @jm.tinkit_class.should respond_to :myGlueEnv
+    @jm.tinkit_class.name.should == "Tinkit::#{@joha_class_name}"
+  end
+end
+
+describe "Using Sample Data" do
+  before(:each) do
+    @joha_class_name = "JohaTestClass"
+    @joha_user = "joha_test_user"
+    @jm = JohaModel.new(@joha_class_name, @joha_user)
+    @best_tree_roots = ["just_a_label", "c"]
+    @node1_params = {:id => "new1", :label => "new1 node",
+                     :description => "A new1 description",
+                     :links => {"http://some.url" => "Url Label"},
+                     :parents => ["c"],
+                     :notes => ["new1 note"],
+                     :update_log => ["created from rspec"] }
+
+   #delete new nodes
+  end
+
+  it "has grapher when initialized" do
+    @jm.grapher.should_not == nil
+    @jm.grapher.should be_a JsivtGrapher
+  end
+
+  it "guesses at best tree roots" do
+    @jm.graphs.size.should == 2
+    @jm.graphs_with_roots.keys.should == @best_tree_roots
+  end
+
+  it "returns tree graph" do
+    @best_tree_roots.each do |root|
+     p @jm.tree_graph(root)
+    end
+  end
+
+  it "can create a new node" do
+    @jm.create_node(@node1_params)
   end
 end
